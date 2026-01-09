5e1e2afa9820b0ffddef539c81b2390ed82feffa
AlbertoCasasDeza <casasdeza@gmail.com>
2026-01-09 11:55:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 from collections import Counter
 
 st.set_page_config(page_title="Planificador Lotes Naturiber", layout="wide")
-st.title("🧠 Planificador de Lotes Salazón Naturiber")
+st.title("🧠 Planificador de Lotes Salazón Naturiber1")
 
 # -------------------------------
 # Panel de configuración (globales)
@@ -1174,4 +1174,5 @@ if uploaded_file is not None:
             file_name="planificacion_lotes.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
+
 
